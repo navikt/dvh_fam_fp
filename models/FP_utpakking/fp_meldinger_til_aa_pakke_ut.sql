9ba3a44a0a66c390ba06a8ec3f465743ad7e9cf9
AArafaR <97885051+AArafaR@users.noreply.github.com>
2026-03-24 15:58:48 +0100

    fjernet ubrukte sources fra schema.yml og la til noen nye

--- models/FP_utpakking/fp_meldinger_til_aa_pakke_ut.sql
+++ models/FP_utpakking/fp_meldinger_til_aa_pakke_ut.sql
@@ -1,15 +1,26 @@
-{{
-    config(
-        materialized='table'
-    )
-}}
+{{ config(
+    materialized = 'table'
+) }}
 
-with fp_meta_data as (
-  select A.pk_fp_meta_data, A.kafka_offset, A.kafka_mottatt_dato, A.kafka_partition, melding,B.KAFKA_OFFSET C from dvh_fam_fp.fam_fp_meta_data A
-  LEFT OUTER JOIN dvh_fam_fp.json_fam_fp_fagsak B ON
-  A.KAFKA_OFFSET=B.KAFKA_OFFSET AND
-  A.KAFKA_PARTITION=B.KAFKA_PARTITION
-  where A.kafka_mottatt_dato >= sysdate - 30
-AND B.KAFKA_OFFSET IS NULL
+WITH fp_meta_data AS (
+
+    SELECT
+        A.pk_fp_meta_data,
+        A.kafka_offset,
+        A.kafka_mottatt_dato,
+        A.kafka_partition,
+        melding,
+        b.kafka_offset C
+    FROM
+        dvh_fam_fp.fam_fp_meta_data A
+        LEFT OUTER JOIN dvh_fam_fp.json_fam_fp_fagsak b
+        ON A.kafka_offset = b.kafka_offset
+        AND A.kafka_partition = b.kafka_partition
+    WHERE
+        A.kafka_mottatt_dato >= SYSDATE - 30
+        AND b.kafka_offset IS NULL
 )
-select * from fp_meta_data
+SELECT
+    *
+FROM
+    fp_meta_data
